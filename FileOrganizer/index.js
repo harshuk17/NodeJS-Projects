83d2f988234d047a3b3b7edee44430626684752e
harshuk17 <harshdhiman8410@gmail.com>
2025-05-11 14:05:38 +0530

    the oragnize folder is created

--- FileOrganizer/index.js
+++ FileOrganizer/index.js
@@ -1,5 +1,7 @@
 let inputArr = process.argv.slice(2);
-console.log(inputArr);
+const fs= require('fs');
+const path= require('path');
+// console.log(inputArr);
 // console.log("helloo");
 
 // example of command 
@@ -24,14 +26,41 @@ switch(command){
 }
 
 function treeFn(dirPath){
-    
 }
 function organizeFn(dirPath){
+    // Step-1: we have to choose the path where we have to oragnize the files \
+    // console.log('dirPAth is :',dirPath);
+    let destinationPath;
+    if(dirPath== undefined){
+        console.log('kindly enter the path')
+        return;
+    }else{
+        let doesExist =fs.existsSync(dirPath);
+        if(doesExist){
+            // Step-2: we have to make an folder named as organize folder 
+             destinationPath=path.join(dirPath,"organized-files");
+            if (!fs.existsSync(destinationPath)) {
+            fs.mkdirSync(destinationPath);
+            console.log("Folder created successfully");
+        }else{
+            console.log('kindly enter the correct path');
+            return;
+        }
+    }
+        oragnizeHelper(dirPath,destinationPath);
+    }
+    // Step-4:  cut/copy the files into the category folder
+    function oragnizeHelper(src,dest){
+        // Step-3: identify the type of files and categorize them 
+        let childNames=fs.readdirSync(src);
+        console.log(childNames);
+     }
 }
 function helpFn(){
     console.log(`
     node indexe.js tree "directoryPath"
     node index.js oragnize "directoryPath"
     node index.js help 
-`)
+    `)
+    // console.log(__dirname);
 }
